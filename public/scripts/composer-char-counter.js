51fb21c68e3e0030ae2d8c6ac038c24a9c01d1c1
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-07-03 15:24:34 -0400

    Fix: Update code to reflect the jquery approapriate syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,11 @@
-
+$(function () {
   $('.tweet-text').on('input', function() {
     const maxLength = 140;
     const currentLength = $(this).val().length;
     const counter = $(this).closest('.new-tweet').find('.counter');
     const remaining = maxLength - currentLength;
 
-    // Update the counter
     counter.text(remaining);
-
-    // Change the color to red if negative
-    if (remaining < 0) {
-      counter.addClass('error');
-    } else {
-      counter.removeClass('error');
-    }
+    counter.toggleClass('error', remaining < 0);
   });
+});
