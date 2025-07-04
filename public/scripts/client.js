6de494ffdc0bac0588cca894040c14fe0a960c73
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-07-04 13:36:54 -0400

    Add separate functions, one to rended the tweet and the other one to create it.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,55 +9,8 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 // Fake data taken from initial-tweets.json
 
-function daysAgo(timestamp) {
-  const old = new Date(timestamp);
-  const now = new Date();
-  const msPerDay = 1000 * 60 * 60 * 24;
-  return Math.floor((now - old) / msPerDay);
-}
 
 $(function() {
-  const renderTweets = function(tweets) {
-    const $container = $('.tweets-container');
-    $container.empty();
-   
-
-    tweets.forEach(tweet => {
-      // Create article structure
-      const $article = $(`
-        <article>
-          <header class="usertweet"> 
-            <div class="user">
-              <div class="nameandimage">
-                <div><img src="${tweet.user.avatars}" alt="avatar"></div>
-                <div class="name"></div>
-              </div>
-              <div class="username"></div>
-            </div>
-            <div class="tweet"></div>
-          </header>
-          <footer>
-            ${daysAgo(tweet.created_at) + " days ago"} <!-- Use timeago.js or custom -->
-            <div class="icons">
-              <i class="fa-solid fa-retweet"></i>
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-heart"></i>
-            </div>
-          </footer>
-        </article>
-      `);
-
-      // Fill in contents
-      $article.find('.name').text(tweet.user.name);
-      $article.find('.username').text(tweet.user.handle);
-      $article.find('.tweet').text(tweet.content.text);
-
-      // Append to container
-      $container.append($article);
-    });
-  };
-
-  // Fake data; replace with real AJAX call
   const data = [
     {
       user: { name: "Newton", avatars: "https://i.imgur.com/73hZDYK.png", handle: "@SirIsaac" },
@@ -70,6 +23,59 @@ $(function() {
       created_at: 1461113959088
     }
   ];
-
   renderTweets(data);
 });
+
+function daysAgo(ts) {
+  return Math.floor((Date.now() - ts) / (1000 * 60 * 60 * 24));
+}
+
+function createTweetElement(tweet) {
+  const $article = $(`
+    <article>
+      <header class="usertweet"> 
+        <div class="user">
+          <div class="nameandimage">
+            <div><img src="${tweet.user.avatars}" alt="avatar"></div>
+            <div class="name"></div>
+          </div>
+          <div class="username"></div>
+        </div>
+      </header>
+      <div class="tweet"></div>
+      <footer>
+        <span class="days-ago" data-ts="${tweet.created_at}"></span>
+        <div class="icons">
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+
+  $article.find('.name').text(tweet.user.name);
+  $article.find('.username').text(tweet.user.handle);
+  $article.find('.tweet').text(tweet.content.text);
+
+  const days = daysAgo(tweet.created_at);
+  $article.find('.days-ago').text(`${days} day${days !== 1 ? 's' : ''} ago`);
+
+  return $article;
+  
+}
+
+
+
+function renderTweets(tweets) {
+  const $container = $('.tweets-container');
+  $container.empty();
+
+  tweets.forEach(t => {
+    const $tweetEl = createTweetElement(t);
+    $container.append($tweetEl);
+  });
+}
+
+
+
